f253c6b8160e64947ac4d96df2a942ed14dbc6cf
Trainer-Project <mohammedalamnyc@gmail.com>
2023-12-03 19:04:30 -0500

    add pagefactory class and base class

--- src/test/java/golbaltek/ny/stepdefintions/LoginStefDef.java
+++ src/test/java/golbaltek/ny/stepdefintions/LoginStefDef.java
@@ -1,44 +1,49 @@
 package golbaltek.ny.stepdefintions;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
+import golbaltek.ny.basepage.BasePage;
+import golbaltek.ny.elements.LoginPage;
+import golbaltek.ny.generic.CommonUtil;
 import io.cucumber.java.en.*;
-import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class LoginStefDef {
-	WebDriver driver;
+
+public class LoginStefDef extends BasePage{
+	LoginPage pf;
 	
 	@Given("User is able to open any browser")
 	public void user_is_able_to_open_any_browser() {
-		WebDriverManager.chromedriver().setup(); 
-	    driver = new ChromeDriver();
-	    driver.manage().window().maximize();
+		BasePage.getInitialization();	
+		pf = new LoginPage();
 	}
 	@Given("User is able to enter the URL")
 	public void user_is_able_to_enter_the_url() {
-		driver.navigate().to("https://magento.softwaretestingboard.com/"); 
-	    
 	}
 	@When("User is able to click on sign in BTN")
 	public void user_is_able_to_click_on_sign_in_btn() {  
-		driver.findElement(By.xpath("//*[@class='page-wrapper']/descendant::a[2]")).click();
-
+		//driver.findElement(By.xpath("//*[@class='page-wrapper']/descendant::a[2]")).click();
+	   
+	   //pf.getClickOnSignIn().click();	   
+	   //CommonUtil.getActionClick(pf.getClickOnSignIn());
+		CommonUtil.getJSClick(pf.getClickOnSignIn());
+	   
 	}
 	@When("User is able to enter the user name")
 	public void user_is_able_to_enter_the_user_name() {
-		driver.findElement(By.xpath("(//*[@class='input-text'])[2]")).sendKeys("tanvirpatwary16@gmail.com");	    
+		//driver.findElement(By.xpath("(//*[@class='input-text'])[2]")).sendKeys(prop.getProperty("userName"));	    
+	pf.getEnterUserName().sendKeys(prop.getProperty("userName"));
 	}
 	@When("User is able to enter the password")
 	public void user_is_able_to_enter_the_password() {
-		driver.findElement(By.xpath("(//*[@class='input-text'])[3]")).sendKeys("Tester01");   
-	    
+		//driver.findElement(By.xpath("(//*[@class='input-text'])[3]")).sendKeys(prop.getProperty("passWord"));   
+	   pf.getEnterPassWord().sendKeys(prop.getProperty("passWord")); 
 	}
 	@When("User is able to click on log in BTN")
 	public void user_is_able_to_click_on_log_in_btn() {
-		driver.findElement(By.xpath("(//*[text()='Sign In'])[1]")).click();
- 	    
+		//driver.findElement(By.xpath("(//*[text()='Sign In'])[1]")).click();
+ 	    //pf.getClickOnSubmit().click();
+ 	   CommonUtil.getActionClick(pf.getClickOnSubmit());
 	}
 	@Then("User is able to verify the user info on the screen")
 	public void user_is_able_to_verify_the_user_info_on_the_screen() throws InterruptedException {
